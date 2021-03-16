384006ae4a689dce268312d1e571289f0e09cc5b
Mateusz Rojek <m.rojek@pureinteractive.pl>
2021-03-16 22:24:31 +0100

    Added tools to lag microphone

--- bundle.js
+++ bundle.js
@@ -1,7 +1,8 @@
 class TeamsTools {
     static sel = {
         buttons: {
-            raiseHand: document.querySelector('#raise-hand-button')
+            raiseHand: document.querySelector('#raise-hand-button'),
+            muteMicrophone: document.querySelector('#microphone-button')
         }
     };
 
@@ -28,6 +29,39 @@ class TeamsTools {
 
         stop() {
             this.data.delay = 0;
+        }
+    }
+
+    static lagMicrophone = {
+        data: {
+            toggled: true
+        },
+
+        start(toggled = true) {
+            if (toggled == true) {
+                this.data.toggled = true;
+            } else if (toggled == false) {
+                this.data.toggled = false;
+            }
+            const muteButton = TeamsTools.sel.buttons.muteMicrophone;
+            let delayParams = [];
+            for(let i = 0; i <= 9; i++) {
+                delayParams.push(Math.round(Math.random() * 800));
+            }
+            console.log(delayParams);
+            
+            const mute = () => {
+                muteButton.click();
+                if (this.data.toggled) {
+                    setTimeout(() => {
+                        mute();
+                    }, delayParams[Math.round(Math.random() * 10)]);
+                }
+            }; mute();
+        },
+
+        stop() {
+            this.start(false);
         }
     }
 }
