9c47fb94688e2f362d5ff02951d7a6791940d755
Mateusz Rojek <m.rojek@pureinteractive.pl>
2021-03-16 16:30:22 +0100

    added raiseHand method

--- bundle.js
+++ bundle.js
@@ -1,3 +1,33 @@
 class TeamsTools {
-    
+    static sel = {
+        buttons: {
+            raiseHand: document.querySelector('#raise-hand-button')
+        }
+    };
+
+    static raiseHand = {
+        data: {
+            delay: 1000
+        },
+
+        setDelay(delay) {
+            this.data.delay = delay;
+        },
+
+        start() {
+            const raiseHandButton = TeamsTools.sel.buttons.raiseHand;
+            const raise = () => {
+                raiseHandButton.click();
+                if (this.data.delay > 0) {
+                    setTimeout(() => {
+                        raise();
+                    }, this.data.delay);
+                }
+            }; raise();
+        },
+
+        stop() {
+            this.data.delay = 0;
+        }
+    }
 }
